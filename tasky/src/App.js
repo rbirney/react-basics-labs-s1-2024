9c48215029ffa134aacefd1c068ec506200166c4
rbirney <rbirney@wit.ie>
2024-09-27 09:47:49 +0100

    Added keys to tasks

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 import Task from './components/Task';
 
 function App() {
-
+  
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
     ]
   });
   
@@ -20,8 +20,9 @@ function App() {
           title={task.title}
           description={task.description}
           deadline={task.deadline}
+          key={task.id}
         />
-      ))} 
+      ))}
     </div>
   );
 }
